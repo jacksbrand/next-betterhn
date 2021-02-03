e515c5502332f3dd01a4e8e63f4e41c7240e3762
Jack Bizzell <jbizzell@protonmail.com>
2021-02-03 21:42:49 +0000

    styling and sorting algo

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,46 @@
 import { useEffect, useState } from "react";
 import fetchData from "./api/fetchData";
 
+import styles from "../styles/base.module.css";
+
 export default function Home() {
 	const [stories, setStories] = useState([]);
+
+	// const sortItems = (array) => {
+	// 	for (let i = 0; i < array.length; i++) {
+	// 		for (let j = 0; j < array.length; j++) {
+	// 			if (array[j].data.score > array[j + 1].data.score) {
+	// 				let temp = array[j];
+	// 				array[j] = array[j + 1];
+	// 				array[j + 1] = temp;
+	// 			}
+	// 		}
+	// 	}
+	// 	return array;
+	// };
 
 	useEffect(async () => {
 		setStories([]);
 		const data = await fetchData();
 
-		setStories(data);
+		const sortedData = data.sort((b, a) => {
+			return a.data.score - b.data.score;
+		});
+
+		setStories(sortedData);
 	}, []);
 
 	return (
-		<div>
+		<div className={styles.container}>
 			{stories.length > 1 ? (
 				<div>
+					<h1 className={styles.h1}>Betterhn</h1>
+					<h2 className={styles.h2}>Powered by the HackerNews API</h2>
+					<p>Shows top stories with at least 50 upvoates, sorted by the same</p>
 					{stories.map((id) => {
-						if (id.data.score > 100) {
+						if (id.data.score > 50) {
 							return (
-								<div>
+								<div className={styles.newsitem}>
 									<a href={id.data.url}>
 										<h1>{id.data.title}</h1>
 									</a>
@@ -31,7 +53,10 @@ export default function Home() {
 					})}
 				</div>
 			) : (
-				<div>Loading</div>
+				<div>
+					Loading an insanely unoptimised website - this will take a second...
+					sorry!
+				</div>
 			)}
 		</div>
 	);
