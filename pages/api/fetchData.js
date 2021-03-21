e5a594a702ddb33f9e5e88d0a198e6c2a37a2717
Jack <jack@localhost.localdomain>
2021-03-21 12:41:28 +0000

    updating colors

--- pages/api/fetchData.js
+++ pages/api/fetchData.js
@@ -2,21 +2,21 @@ const fetch = require("node-fetch");
 const axios = require("axios");
 
 export default async function fetchData(req, res) {
-	const { data } = await axios.get(
-		"https://hacker-news.firebaseio.com/v0/topstories.json"
-	);
+  const { data } = await axios.get(
+    "https://hacker-news.firebaseio.com/v0/topstories.json"
+  );
 
-	const spliced = data.splice(0, 200);
+  const spliced = data.splice(0, 200);
 
-	const oranges = spliced.map(async (id) => {
-		return await axios.get(
-			`https://hacker-news.firebaseio.com/v0/item/${id}.json`
-		);
-	});
+  const oranges = spliced.map(async (id) => {
+    return await axios.get(
+      `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+    );
+  });
 
-	const storiesArray = Promise.all(oranges).then((res) => {
-		return res;
-	});
+  const storiesArray = Promise.all(oranges).then((res) => {
+    return res;
+  });
 
-	return storiesArray;
+  return storiesArray;
 }
--- pages/index.js
+++ pages/index.js
@@ -4,60 +4,64 @@ import fetchData from "./api/fetchData";
 import styles from "../styles/base.module.css";
 
 export default function Home() {
-	const [stories, setStories] = useState([]);
+  const [stories, setStories] = useState([]);
 
-	// const sortItems = (array) => {
-	// 	for (let i = 0; i < array.length; i++) {
-	// 		for (let j = 0; j < array.length; j++) {
-	// 			if (array[j].data.score > array[j + 1].data.score) {
-	// 				let temp = array[j];
-	// 				array[j] = array[j + 1];
-	// 				array[j + 1] = temp;
-	// 			}
-	// 		}
-	// 	}
-	// 	return array;
-	// };
+  // const sortItems = (array) => {
+  // 	for (let i = 0; i < array.length; i++) {
+  // 		for (let j = 0; j < array.length; j++) {
+  // 			if (array[j].data.score > array[j + 1].data.score) {
+  // 				let temp = array[j];
+  // 				array[j] = array[j + 1];
+  // 				array[j + 1] = temp;
+  // 			}
+  // 		}
+  // 	}
+  // 	return array;
+  // };
 
-	useEffect(async () => {
-		setStories([]);
-		const data = await fetchData();
+  useEffect(async () => {
+    setStories([]);
+    const data = await fetchData();
 
-		const sortedData = data.sort((b, a) => {
-			return a.data.score - b.data.score;
-		});
+    const sortedData = data.sort((b, a) => {
+      return a.data.score - b.data.score;
+    });
 
-		setStories(sortedData);
-	}, []);
+    setStories(sortedData);
+  }, []);
 
-	return (
-		<div className={styles.container}>
-			{stories.length > 1 ? (
-				<div>
-					<h1 className={styles.h1}>Betterhn</h1>
-					<h2 className={styles.h2}>Powered by the HackerNews API</h2>
-					<p>Shows top stories with at least 50 upvoates, sorted by the same</p>
-					{stories.map((id) => {
-						if (id.data.score > 50) {
-							return (
-								<div className={styles.newsitem}>
-									<a href={id.data.url}>
-										<h1>{id.data.title}</h1>
-									</a>
-									<p>{id.data.score}</p>
-								</div>
-							);
-						} else {
-							return null;
-						}
-					})}
-				</div>
-			) : (
-				<div>
-					Loading an insanely unoptimised website - this will take a second...
-					sorry!
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      {stories.length > 1 ? (
+        <div>
+          <h1 className={styles.h1}>Betterhn</h1>
+          <h2 className={styles.h2}>Powered by the HackerNews API</h2>
+          <p className={styles.p}>
+            Shows top stories with at least 50 upvoates, sorted by the same
+          </p>
+          {stories.map((id) => {
+            if (id.data.score > 50) {
+              return (
+                <div className={styles.newsitem}>
+                  <a href={id.data.url}>
+                    <h1>{id.data.title}</h1>
+                  </a>
+                  <p>{id.data.score}</p>
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      ) : (
+        <div>
+          <p className={styles.p}>
+            Loading an insanely unoptimised website - this will take a second...
+            sorry!
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
